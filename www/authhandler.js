999b078f7e6d260b76656afd9ba73b6becf227a2
Matteo Prinetti <prinex@gmail.com>
2021-11-11 16:44:09 +0100

    fix js interface

--- www/authhandler.js
+++ www/authhandler.js
@@ -1,13 +1,17 @@
-AuthHandler = function(seed, success, error) {
-	if(cordova.platformId == "android"){
-		cordova.exec(success, error, "AuthHandler", "setSeed", [seed]);
+cordova.define("cordova-plugin-customlogin.AuthHandler", function (require, exports, module) {
+	AuthHandler = function (seed, success, error) {
+		if (cordova.platformId == "android") {
+			cordova.exec(success, error, "AuthHandler", "setSeed", [seed]);
+		}
 	}
-}
 
-if(!cordova.plugins) {
-	cordova.plugins = {};
-}
-if(!cordova.plugins.AuthHandler) {
-	cordova.plugins.AuthHandler = AuthHandler;
-}
+	if (!cordova.plugins) {
+		cordova.plugins = {};
+	}
+	if (!cordova.plugins.AuthHandler) {
+		cordova.plugins.AuthHandler = AuthHandler;
+	}
 
+
+});
+;
